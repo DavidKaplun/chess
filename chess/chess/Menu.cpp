2ffe2a9a30fbc297fd44636482b7540b6d2425fa
David Kaplun <davidkaplun505@gmail.com>
2021-12-10 07:06:05 +0200

    added bishops to the board

--- chess/chess/Menu.cpp
+++ chess/chess/Menu.cpp
@@ -37,18 +37,28 @@ void createBoard(std::vector<std::vector<Piece*>>& board)
 void addPiecesToBoard(std::vector<std::vector<Piece*>>& board)
 {
 	//white rooks
-	board[7][0]=new Rook('a','1','r','w');
+	board[7][0] = new Rook('a', '1', 'r', 'w');
 	board[7][7] = new Rook('h', '1', 'r', 'w');
 
 	//black rooks
 	board[0][0] = new Rook('a', '8', 'r', 'b');
 	board[0][7] = new Rook('h', '8', 'r', 'b');
 
+
 	//white knights
-	board[7][1] = new Knight('b', '1', 'k', 'w');
-	board[7][6] = new Knight('g', '1', 'k', 'w');
+	board[7][1] = new Knight('b', '1', 'n', 'w');
+	board[7][6] = new Knight('g', '1', 'n', 'w');
 
 	//black knights
-	board[0][1] = new Knight('b', '8', 'k', 'b')
-	board[0][6] = new Knight('g', '8', 'k', 'b');
+	board[0][1] = new Knight('b', '8', 'n', 'b');
+	board[0][6] = new Knight('g', '8', 'n', 'b');
+
+
+	//white bishops
+	board[7][2] = new Knight('c', '1', 'b', 'w');
+	board[7][5] = new Knight('f', '1', 'b', 'w');
+
+	//black bishops
+	board[0][2] = new Knight('c', '8', 'b', 'b');
+	board[0][5] = new Knight('f', '8', 'b', 'b');
 }
