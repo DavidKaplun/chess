237e775b361ced455fafea239a111e0c38962e67
David Kaplun <davidkaplun505@gmail.com>
2021-12-10 06:16:06 +0200

    never mind, I just forgot to put ';' at the end of the class

--- chess/chess/Queen.h
+++ chess/chess/Queen.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "Piece.h"
 
-class Queen:public Piece
+class Queen :public Piece
 {
+
 public:
 	Queen(char x, char y, char type, char color);
 	~Queen();
 	virtual void change_pos_moves(std::vector<std::vector<Piece*>> board) override;
-}
+};
