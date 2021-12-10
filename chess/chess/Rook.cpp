924c3e9811ca5af9ef8a5cae8ee150d81433757e
David Kaplun <davidkaplun505@gmail.com>
2021-12-10 05:58:31 +0200

    added isFirstMove for the rook and now I'm going to add to the pawn

--- chess/chess/Rook.cpp
+++ chess/chess/Rook.cpp
@@ -2,7 +2,7 @@
 
 Rook::Rook(char x, char y, char type, char color) : Piece(x,y,type,color)
 {
-	
+	this->_isFirstMove = true;
 }
 
 Rook::~Rook()
--- chess/chess/Rook.h
+++ chess/chess/Rook.h
@@ -4,7 +4,8 @@
 
 class Rook : public Piece
 {
-
+private:
+	bool _isFirstMove;
 public:
 	Rook(char x, char y, char type, char color);
 	~Rook();
