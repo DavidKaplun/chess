17e1543bcfa64af4baa7b4d7431593ba46e183a0
David Kaplun <davidkaplun505@gmail.com>
2021-12-10 08:53:19 +0200

    fliped the board(now white is at the top and black is at the bottom

--- chess/chess/Menu.cpp
+++ chess/chess/Menu.cpp
@@ -36,43 +36,43 @@ void createBoard(std::vector<std::vector<Piece*>>& board)
 
 void addPiecesToBoard(std::vector<std::vector<Piece*>>& board)
 {
-	//white rooks
-	board[7][0] = new Rook('a', '1', 'r', 'w');
-	board[7][7] = new Rook('h', '1', 'r', 'w');
-
 	//black rooks
-	board[0][0] = new Rook('a', '8', 'r', 'b');
-	board[0][7] = new Rook('h', '8', 'r', 'b');
+	board[7][0] = new Rook('a', '8', 'r', 'b');
+	board[7][7] = new Rook('h', '8', 'r', 'b');
 
+	//white rooks
+	board[0][0] = new Rook('a', '1', 'r', 'w');
+	board[0][7] = new Rook('h', '1', 'r', 'w');
 
-	//white knights
-	board[7][1] = new Knight('b', '1', 'n', 'w');
-	board[7][6] = new Knight('g', '1', 'n', 'w');
 
 	//black knights
-	board[0][1] = new Knight('b', '8', 'n', 'b');
-	board[0][6] = new Knight('g', '8', 'n', 'b');
+	board[7][1] = new Knight('b', '8', 'n', 'b');
+	board[7][6] = new Knight('g', '8', 'n', 'b');
 
+	//white knights
+	board[0][1] = new Knight('b', '1', 'n', 'w');
+	board[0][6] = new Knight('g', '1', 'n', 'w');
 
-	//white bishops
-	board[7][2] = new Bishop('c', '1', 'b', 'w');
-	board[7][5] = new Bishop('f', '1', 'b', 'w');
 
 	//black bishops
-	board[0][2] = new Bishop('c', '8', 'b', 'b');
-	board[0][5] = new Bishop('f', '8', 'b', 'b');
+	board[7][2] = new Bishop('c', '8', 'b', 'b');
+	board[7][5] = new Bishop('f', '8', 'b', 'b');
 
+	//white bishops
+	board[0][2] = new Bishop('c', '1', 'b', 'w');
+	board[0][5] = new Bishop('f', '1', 'b', 'w');
 
-	//white queen
-	board[7][3] = new Queen('d', '1', 'q', 'w');
 
 	//black queen
-	board[0][3] = new Queen('d', '8', 'q', 'b');
+	board[7][3] = new Queen('d', '8', 'q', 'b');
+
+	//white queen
+	board[0][3] = new Queen('d', '1', 'q', 'w');
 
 	
-	//white king
-	board[7][4] = new King('e', '1', 'k', 'w');
-
 	//black king
-	board[0][4] = new King('e', '8', 'k', 'b');
+	board[7][4] = new King('e', '8', 'k', 'b');
+
+	//white king
+	board[0][4] = new King('e', '1', 'k', 'w');
 }
