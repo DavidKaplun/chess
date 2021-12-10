6bca0fdf080e795ee08c6d52c044556c587e73d3
David Kaplun <davidkaplun505@gmail.com>
2021-12-10 06:14:48 +0200

    added queen but it gives me bullshit error that i'm going to fix now

---
+++ chess/chess/Queen.cpp
@@ -0,0 +1,32 @@
+#include "Queen.h"
+
+Queen::Queen(char x, char y, char type, char color) :Piece(x,y,type,color)
+{
+
+}
+
+Queen::~Queen()
+{
+
+}
+
+void Queen::change_pos_moves(std::vector<std::vector<Piece*>> board)
+{
+	int current_x = int(this->_posX) - 97;
+	int current_y = int(this->_posY) - 48;
+	// e7
+	// current_x = 101-97 = 4
+	// board[8-current_x = 4] = e
+	// current_y = 55-48 = 7
+	// board[8-current_x = 1] = 7      
+	/*
+	8 - 0
+	7 - 1
+	6 - 2
+	5 - 3
+	4 - 4
+	3 - 5
+	2 - 6
+	1 - 7
+	*/
+}
---
+++ chess/chess/Queen.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Piece.h"
+
+class Queen:public Piece
+{
+public:
+	Queen(char x, char y, char type, char color);
+	~Queen();
+	virtual void change_pos_moves(std::vector<std::vector<Piece*>> board) override;
+}
