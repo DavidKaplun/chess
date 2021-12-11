25dbbea91c28dd5035abd03eebe4de493f0a85aa
David Kaplun <davidkaplun505@gmail.com>
2021-12-11 04:57:26 +0200

    added function that checks if there is check every move from rooks or queen

--- chess/chess/King.cpp
+++ chess/chess/King.cpp
@@ -1,5 +1,8 @@
 #include "King.h"
 
+#define BOARD_HEIGHT 8
+#define BOARD_WIDTH 8
+
 King::King(char type, char color) :Piece(type, color)
 {
 
@@ -18,10 +21,150 @@ bool King::isMovePossible(int source_x, int source_y, int destination_x, int des
 
 bool King::isInDangerAfterMove(int source_x, int source_y, int destination_x, int destination_y, std::vector<std::vector<Piece*>> board)
 {
-	return true;
+	return false;
 }
 
 bool King::isInCheckFromRook(int source_x, int source_y, int destination_x, int destination_y, std::vector<std::vector<Piece*>> board)
 {
-	return true;
+	int x = 0;
+	int y = 0;
+
+	int kingsX = this->getX();
+	int kingsY = this->getY();
+
+
+	for (x = kingsX; x < BOARD_WIDTH; x++)//this for loop checks everything to the right of the king
+	{
+		if (x != source_x)//we need to address this square as empty because after the move its going to be empty
+		{
+			if (x == destination_x and kingsY == destination_y)
+			{
+				break;//breaks the loop because the piece blocks the king from check in this direction
+			}
+			else
+			{
+				if (board[kingsY][x] != nullptr)//checks if the square is not empty
+				{
+					if (board[kingsY][x]->getColor() == this->getColor())
+					{
+						break;//because the piece thats on the square is the same color and it covers the check
+					}
+					else
+					{
+						char type = board[kingsY][x]->getType();//just to make the if statement shorter
+						if (type == 'Q' or type == 'q' or type == 'r' or type == 'R')
+						{
+							return true;//if the piece on this square is a queen or a rook then its a check
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	for (x = kingsX; 0<=x; x--)//this for loop checks everything to the left of the king
+	{
+		if (x != source_x)//we need to address this square as empty because after the move its going to be empty
+		{
+			if (x == destination_x and kingsY == destination_y)
+			{
+				break;//breaks the loop because the piece blocks the king from check in this direction
+			}
+			else
+			{
+				if (board[kingsY][x] != nullptr)//checks if the square is not empty
+				{
+					if (board[kingsY][x]->getColor() == this->getColor())
+					{
+						break;//because the piece thats on the square is the same color and it covers the check
+					}
+					else
+					{
+						char type = board[kingsY][x]->getType();//just to make the if statement shorter
+						if (type == 'Q' or type == 'q' or type == 'r' or type == 'R')
+						{
+							return true;//if the piece on this square is a queen or a rook then its a check
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	for (y = kingsY; y < BOARD_HEIGHT; y++)//checks every square below the king(remember the index of the squares at the top is lower then at the bottom)
+	{
+		if (source_y != y)//we need to address this square as empty because after the move its going to be empty
+		{
+			if (y == destination_y and kingsX == destination_x)
+			{
+				break;//breaks the loop because the piece blocks the king from check in this direction
+			}
+			else
+			{
+				if (board[y][kingsX] != nullptr)//checks if the square is not empty
+				{
+					if (board[y][kingsX]->getColor() == this->getColor())
+					{
+						break;//because the piece thats on the square is the same color and it covers the check
+					}
+					else
+					{
+						char type = board[y][kingsX]->getType();//just to make the if statement shorter
+						if (type == 'Q' or type == 'q' or type == 'r' or type == 'R')
+						{
+							return true;//if the piece on this square is a queen or a rook then its a check
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (y = kingsY; 0<=y; y--)//checks every square above the king(remember the index of the squares at the top is lower then at the bottom)
+	{
+		if (source_y != y)//we need to address this square as empty because after the move its going to be empty
+		{
+			if (y == destination_y and kingsX == destination_x)
+			{
+				break;//breaks the loop because the piece blocks the king from check in this direction
+			}
+			else
+			{
+				if (board[y][kingsX] != nullptr)//checks if the square is not empty
+				{
+					if (board[y][kingsX]->getColor() == this->getColor())
+					{
+						break;//because the piece thats on the square is the same color and it covers the check
+					}
+					else
+					{
+						char type = board[y][kingsX]->getType();//just to make the if statement shorter
+						if (type == 'Q' or type == 'q' or type == 'r' or type == 'R')
+						{
+							return true;//if the piece on this square is a queen or a rook then its a check
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+
+	return false;
+}
+
+
+
+
+int King::getX()
+{
+	return this->_posX;
+}
+
+int King::getY()
+{
+	return this->_posY;
 }
--- chess/chess/King.h
+++ chess/chess/King.h
@@ -5,11 +5,13 @@ class King :public Piece
 {
 private:
 	int _posX;
-	int _poxY;
+	int _posY;
 public:
 	King(char type, char color);
 	~King();
 	virtual bool isMovePossible(int source_x, int source_y, int destination_x, int destination_y, std::vector<std::vector<Piece*>> board) override;
 	bool isInDangerAfterMove(int source_x, int source_y, int destination_x, int destination_y, std::vector<std::vector<Piece*>> board);
 	bool isInCheckFromRook(int source_x, int source_y, int destination_x, int destination_y, std::vector<std::vector<Piece*>> board);
+	int getY();
+	int getX();
 };
