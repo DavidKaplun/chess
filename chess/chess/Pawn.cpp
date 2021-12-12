d3a024bc39923607426c122820bc515297af3efd
David Kaplun <davidkaplun505@gmail.com>
2021-12-12 19:09:28 +0200

    added isMovePos for da pawn

--- chess/chess/Pawn.cpp
+++ chess/chess/Pawn.cpp
@@ -12,7 +12,95 @@ Pawn::~Pawn()
 
 bool Pawn::isMovePossible(int source_x, int source_y, int destination_x, int destination_y, std::vector<std::vector<Piece*>> board)
 {
-	return 1;
+	if (this->getColor() == 'w')
+	{
+		if (destination_y == source_y + 1 and source_x == destination_x)
+		{
+			if (board[destination_y][destination_x] == nullptr)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if(this->_isFirstMove==true and destination_y==source_y+2 and source_x==destination_x)
+		{
+			if (board[destination_y][destination_x] == nullptr and board[destination_y - 1][destination_x] == nullptr)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (destination_y == source_y + 1 and abs(source_x - destination_x) == 1)
+		{
+			if (board[destination_y][destination_x] != nullptr)
+			{
+				if (board[destination_y][destination_x]->getColor() != this->getColor())
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+
+	else if (this->getColor() == 'b')
+	{
+		if (destination_y == source_y - 1 and source_x == destination_x)
+		{
+			if (board[destination_y][destination_x] == nullptr)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (this->_isFirstMove == true and destination_y == source_y - 2 and source_x == destination_x)
+		{
+			if (board[destination_y][destination_x] == nullptr and board[destination_y + 1][destination_x] == nullptr)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (destination_y == source_y - 1 and abs(source_x - destination_x) == 1)
+		{
+			if (board[destination_y][destination_x] != nullptr)
+			{
+				if (board[destination_y][destination_x]->getColor() != this->getColor())
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
+	}
 }
 
 void Pawn::deletePiece()
