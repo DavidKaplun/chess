9687d8b5c2fdb44a259f40c62057d16d19d5d771
David Kaplun <davidkaplun505@gmail.com>
2021-12-12 05:42:55 +0200

    added isMovePossible for bishop

--- chess/chess/Bishop.cpp
+++ chess/chess/Bishop.cpp
@@ -1,5 +1,7 @@
 #include "Bishop.h"
 
+#define BOARD_WIDTH 8
+#define BOARD_HEIGHT 8
 Bishop::Bishop(char type, char color) :Piece(type, color)
 {
 
@@ -16,5 +18,164 @@ bool Bishop::isMovePossible(int source_x, int source_y, int destination_x, int d
 	{
 		return false;
 	}
+	
+	int x, y;
+	if (destination_x > source_x and destination_y > source_y)
+	{
+
+
+		x = source_x + 1;
+		y = source_y + 1;
+		while (x < BOARD_WIDTH and y < BOARD_HEIGHT)
+		{
+			if (x == destination_x and y == destination_y)
+			{
+				if (board[y][x] != nullptr)
+				{
+					if (board[y][x]->getColor() == this->getColor())
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if (board[y][x] != nullptr)
+				{
+					std::cout << "Bishops can't jump over pieces" << std::endl;
+					return false;
+				}
+			}
+			x++;
+			y++;
+		}
+	}
+
+	else if (destination_x > source_x and destination_y < source_y)
+	{
+
+
+		x = source_x + 1;
+		y = source_y - 1;
+		while (x < BOARD_WIDTH and 0<=y)
+		{
+			if (x == destination_x and y == destination_y)
+			{
+				if (board[y][x] != nullptr)
+				{
+					if (board[y][x]->getColor() == this->getColor())
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if (board[y][x] != nullptr)
+				{
+					std::cout << "Bishops can't jump over pieces" << std::endl;
+					return false;
+				}
+			}
+			x++;
+			y--;
+		}
+	}
+
+	else if (destination_x < source_x and destination_y > source_y)
+	{
+
+
+		x = source_x - 1;
+		y = source_y + 1;
+		while (0<=x and y < BOARD_HEIGHT)
+		{
+			if (x == destination_x and y == destination_y)
+			{
+				if (board[y][x] != nullptr)
+				{
+					if (board[y][x]->getColor() == this->getColor())
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if (board[y][x] != nullptr)
+				{
+					std::cout << "Bishops can't jump over pieces" << std::endl;
+					return false;
+				}
+			}
+			x--;
+			y++;
+		}
+	}
+
+	else if (destination_x < source_x and destination_y < source_y)
+	{
+
+
+		x = source_x - 1;
+		y = source_y - 1;
+		while (0 <= x and 0<=y)
+		{
+			if (x == destination_x and y == destination_y)
+			{
+				if (board[y][x] != nullptr)
+				{
+					if (board[y][x]->getColor() == this->getColor())
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if (board[y][x] != nullptr)
+				{
+					std::cout << "Bishops can't jump over pieces" << std::endl;
+					return false;
+				}
+			}
+			x--;
+			y--;
+		}
+	}
+
+	std::cout << "This shouldn't happen fix me!" << std::endl;
 }
 
